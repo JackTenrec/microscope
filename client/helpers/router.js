40d63180b3b4defcd14d32948d2a712b9a6322d3
mnl06 <maserna06@gmail.com>
2018-03-06 11:08:36 -0500

    Commit 5-3-1
    Fixed Routing to a single post page.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -6,7 +6,7 @@ Router.map(function() {
   this.route('postsList', {path: '/'});
 
   this.route('postPage', {
-    path: '/posts/:_id',
-    data: function() { return Posts.findOne(this.params._id); }
+    path: 'posts/:_id',
+    data: function() { return Posts.findOne({_id: this.params._id}); }
   });
 });
