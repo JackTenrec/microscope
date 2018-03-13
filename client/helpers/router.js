094b69f529f5f599e8815146cf0821e61f66a60b
mnl06 <maserna06@gmail.com>
2018-03-12 22:38:13 -0500

    Commit 6-1.
    Added accounts and added template to the header.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,12 +1,12 @@
-Router.configure({
-  layoutTemplate: 'layout'
-});
-
-Router.map(function() {
-  this.route('postsList', {path: '/'});
-
-  this.route('postPage', {
-    path: 'posts/:_id',
-    data: function() { return Posts.findOne({_id: this.params._id}); }
-  });
-});
+// Router.configure({
+//   layoutTemplate: 'layout'
+// });
+//
+// Router.map(function() {
+//   this.route('postsList', {path: '/'});
+//
+//   this.route('postPage', {
+//     path: 'posts/:_id',
+//     data: function() { return Posts.findOne({_id: this.params._id}); }
+//   });
+// });
